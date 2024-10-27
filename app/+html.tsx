ffc7f341ed4ba76346032bb7cdcef061cd9c0077
Saliou Diallo <lavaleur1@hotmail.com>
2024-10-27 12:15:42 +0000

    Updated .gitignore and made project changes

--- app/+html.tsx
+++ app/+html.tsx
@@ -23,7 +23,11 @@ export default function Root({ children }: PropsWithChildren) {
         <style dangerouslySetInnerHTML={{ __html: responsiveBackground }} />
         {/* Add any additional <head> elements that you want globally available on web... */}
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Display Hello Saliou */}
+        <h1>Hello Saliou</h1>
+        {children}
+      </body>
     </html>
   );
 }
